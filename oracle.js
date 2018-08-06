847e7fa0f50e5d7349653527e5fc1d3a650a2256
Mark Rudolph <mark_rudolph@yahoo.com>
2018-08-06 10:51:11 -0700

    corrects names in oracle.js: oracle to subscription

--- oracle.js
+++ oracle.js
@@ -1,5 +1,5 @@
 var fetch = require('fetch')
-var OracleContract = require('./build/contracts/Subscription.json')
+var SubscriptionContract = require('./build/contracts/Subscription.json')
 var contract = require('truffle-contract')
 
 var Web3 = require('web3');
@@ -10,23 +10,23 @@ var prev_price = 0;
 
 // Truffle abstraction to interact with our
 // deployed contract
-var oracleContract = contract(OracleContract)
-oracleContract.setProvider(web3.currentProvider)
+var subscriptionContract = contract(SubscriptionContract)
+subscriptionContract.setProvider(web3.currentProvider)
 
 // Dirty hack for web3@1.0.0 support for localhost testrpc
 // see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
-if (typeof oracleContract.currentProvider.sendAsync !== "function") {
-  oracleContract.currentProvider.sendAsync = function() {
-    return oracleContract.currentProvider.send.apply(
-      oracleContract.currentProvider, arguments
+if (typeof subscriptionContract.currentProvider.sendAsync !== "function") {
+  subscriptionContract.currentProvider.sendAsync = function() {
+    return subscriptionContract.currentProvider.send.apply(
+      subscriptionContract.currentProvider, arguments
     );
   };
 }
 
 // Get accounts from web3
 web3.eth.getAccounts((err, accounts) => {
-  oracleContract.deployed()
-  .then((oracleInstance) => {
+  subscriptionContract.deployed()
+  .then((subscriptionInstance) => {
 
     // schedule 60-second priceETHUDS update in subscription contract
     setInterval(() => {
@@ -41,12 +41,12 @@ web3.eth.getAccounts((err, accounts) => {
           console.log(`oracle reads priceETHUSD = $${eth_price}`);
 
           // Send data back contract on-chain
-          oracleInstance.callback(eth_price, {from: accounts[0]})
+          subscriptionInstance.callback(eth_price, {from: accounts[0]})
       })
     }, 60000);
 
     // Watch event and respond to event PriceQuery with callback  
-    oracleInstance.PriceQuery()
+    subscriptionInstance.PriceQuery()
     .watch((err, event) => {
       console.log(`\noracle receives event PriceQuery!`);
 
@@ -59,12 +59,12 @@ web3.eth.getAccounts((err, accounts) => {
           console.log(`oracle reads priceETHUSD = $${eth_price}`);
 
           // Send data back contract on-chain
-          oracleInstance.callback(eth_price, {from: accounts[0]})
+          subscriptionInstance.callback(eth_price, {from: accounts[0]})
       })
     })
 
     // Watch event Price and display current price
-    oracleInstance.Price()
+    subscriptionInstance.Price()
     .watch((err, event) => {
       let b = (event.args.price - prev_price);
       if(b !== 0){
@@ -74,7 +74,7 @@ web3.eth.getAccounts((err, accounts) => {
     })
 
     // Watch event and respond to event Registered and begin reminder tracking 
-    oracleInstance.Registered()
+    subscriptionInstance.Registered()
     .watch((err, event) => {
       //let subscriber = event.args.subscriber;
       let email = event.args.email;
